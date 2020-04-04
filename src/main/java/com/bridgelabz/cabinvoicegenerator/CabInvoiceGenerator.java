78f46373839351d81782631acbe7009f5b160d51
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-04 19:11:59 +0530

    ADDED: Constant for minimum fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,15 +1,19 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+
+    //CONSTANTS
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final int MINIMUM_FARE = 5;
 
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO CAB INVOICE GENERATOR /**************************/");
     }
 
+    //METHOD TO CALCULATE TOTAL FARE FOR JOURNEY
     public double calculateTotalFare(double distanceInKm, int journeyTimeInMinutes) {
         double totalFare = distanceInKm * COST_PER_KILOMETER + journeyTimeInMinutes * COST_PER_MINUTE;
-        return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
