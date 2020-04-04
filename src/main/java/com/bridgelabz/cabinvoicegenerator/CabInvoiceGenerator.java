fec0c0e02edd0bae5ef4fb7d1d7b1e7a6327d6ad
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-04 18:43:45 +0530

    ADDED: Method to calculate total fare for journey

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO CAB INVOICE GENERATOR /**************************/");
+    }
+
+    public double calculateTotalFare(double distanceInKm, int journeyTimeInMinutes) {
+        double totalFare = distanceInKm * COST_PER_KILOMETER + journeyTimeInMinutes * COST_PER_MINUTE;
+        return totalFare;
     }
 }
