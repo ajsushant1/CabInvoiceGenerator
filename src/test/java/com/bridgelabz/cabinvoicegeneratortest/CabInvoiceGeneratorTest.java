cd26be856cfc6b41e04befe80919ad14205c9b39
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-04 18:41:05 +0530

    ADDED: Test case method to check total fare of the journey

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.cabinvoicegeneratortest;
 
 import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
     CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+    @Test
+    public void givenDistanceAndTime_GenerateTotalFare_ShouldReturnTheTotalFareForTheJourney() {
+        double totalFare = cabInvoiceGenerator.calculateTotalFare(10.55, 30);
+        Assert.assertEquals(135.5, totalFare, 0);
+    }
 }
