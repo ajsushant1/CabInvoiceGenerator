293a7ada0039e9972a1b2cdc00889923a3bb2c3d
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-06 18:36:53 +0530

    ADDED: Test case method to check totalfare of ride according to ride category

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -11,28 +11,28 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void givenDistanceAndTime_GenerateTotalFare_ShouldReturnTheTotalFareForTheJourney() {
-        Rides[] rides = {new Rides(10.55, 30)};
+        Rides[] rides = {new Rides(Rides.RideType.NORMAL,10.55, 30)};
         double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
         Assert.assertEquals(135.5, totalFare, 0);
     }
 
     @Test
     public void givenDistanceAndTime_WhenTotalFareLessThanMinimumFare_ShouldReturnFixedMinimumFare() {
-        Rides[] rides = {new Rides(0.195, 2)};
+        Rides[] rides = {new Rides(Rides.RideType.NORMAL,0.195, 2)};
         double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
         Assert.assertEquals(5, totalFare, 0);
     }
 
     @Test
     public void givenMultipleRides_GenerateTotalFare_ShouldReturnTheTotalFareForTheJourney() {
-        Rides[] rides = {new Rides(35.0, 45), new Rides(10.55, 30)};
+        Rides[] rides = {new Rides(Rides.RideType.NORMAL,35.0, 45), new Rides(Rides.RideType.PREMIUM,10.55, 30)};
         double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
-        Assert.assertEquals(530.5, totalFare, 0);
+        Assert.assertEquals(613.25, totalFare, 0);
     }
 
     @Test
     public void givenMultipleRides_GenerateTotalFare_ShouldReturnInvoiceSummery() {
-        Rides[] rides = {new Rides(35.0, 45), new Rides(10.55, 30)};
+        Rides[] rides = {new Rides(Rides.RideType.NORMAL,35.0, 45), new Rides(Rides.RideType.NORMAL,10.55, 30)};
         cabInvoiceGenerator.addRides("user@1", rides);
         InvoiceSummery invoiceSummery = cabInvoiceGenerator.getInvoiceSummery("user@1");
         InvoiceSummery expectedSummery = new InvoiceSummery(2, 530.5);
@@ -41,10 +41,24 @@ public class CabInvoiceGeneratorTest {
 
     @Test
     public void givenUserId_GenerateTotalFare_ShouldReturnInvoiceSummery() {
-        Rides[] rides = {new Rides(35.0, 45), new Rides(10.55, 30), new Rides(20, 30)};
+        Rides[] rides = {new Rides(Rides.RideType.PREMIUM,35.0, 45), new Rides(Rides.RideType.NORMAL,10.55, 30), new Rides(Rides.RideType.NORMAL,20, 30)};
         cabInvoiceGenerator.addRides("user@1", rides);
         InvoiceSummery invoiceSummery = cabInvoiceGenerator.getInvoiceSummery("user@1");
-        InvoiceSummery expectedSummery = new InvoiceSummery(3, 760.5);
+        InvoiceSummery expectedSummery = new InvoiceSummery(3, 980.5);
         Assert.assertEquals(expectedSummery, invoiceSummery);
+    }
+
+    @Test
+    public void givenDistanceAndTimeWithRideCategoryNormal_GenerateTotalFare_ShouldReturnTheTotalFareForTheJourney() {
+        Rides[] rides = {new Rides(Rides.RideType.NORMAL,21.55, 30)};
+        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+        Assert.assertEquals(245.5,totalFare,0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeWithRideCategoryPremium_GenerateTotalFare_ShouldReturnTheTotalFareForTheJourney() {
+        Rides[] rides = {new Rides(Rides.RideType.PREMIUM,21.55, 30)};
+        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+        Assert.assertEquals(383.25,totalFare,0);
     }
 }
