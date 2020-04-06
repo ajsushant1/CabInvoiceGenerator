c7f51a5056f2c80bf70f512a4a6f6732d1785279
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-06 18:43:14 +0530

    REFACTORED: calculateTotalFare mehtod to calculate totalfare according to ride catagory

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceService.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceService.java
@@ -6,6 +6,9 @@ public class InvoiceService {
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
     private static final int MINIMUM_FARE = 5;
+    private static final double PREMIUM_COST_PER_KILOMETER = 15;
+    private static final int PREMIUM_COST_PER_MINUTE = 2;
+    private static final int PREMIUM_MINIMUM_FARE = 20;
     RideRepository rideRepository;
 
     public InvoiceService() {
@@ -18,11 +21,19 @@ public class InvoiceService {
 
     //METHOD TO CALCULATE TOTAL FARE FOR JOURNEY
     public double calculateTotalFare(Rides[] rides) {
-        double totalFare = 0;
+        double totalFare = 0, rideFare;
         for (Rides ride : rides) {
-            totalFare += ride.distanceInKm * COST_PER_KILOMETER + ride.journeyTimeInMinutes * COST_PER_MINUTE;
+            rideFare = 0;
+            if (ride.rideType.equals(Rides.RideType.NORMAL)) {
+                rideFare = ride.distanceInKm * COST_PER_KILOMETER + ride.journeyTimeInMinutes * COST_PER_MINUTE;
+                totalFare += Math.max(MINIMUM_FARE, rideFare);
+            }
+            if (ride.rideType.equals(Rides.RideType.PREMIUM)) {
+                rideFare += ride.distanceInKm * PREMIUM_COST_PER_KILOMETER + ride.journeyTimeInMinutes * PREMIUM_COST_PER_MINUTE;
+                totalFare += Math.max(PREMIUM_MINIMUM_FARE, rideFare);
+            }
         }
-        return Math.max(totalFare, MINIMUM_FARE);
+        return totalFare;
     }
 
     //METHOD TO GET INVOICE SUMMERY
